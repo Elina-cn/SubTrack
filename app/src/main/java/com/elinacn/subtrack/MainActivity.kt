276e3bf0fcc5b33bbb29c34ec96cf4cc5241e9df
Elina <cane74541@gmail.com>
2026-02-26 23:54:55 +0300

    yorum satırı silindi

--- app/src/main/java/com/elinacn/subtrack/MainActivity.kt
+++ app/src/main/java/com/elinacn/subtrack/MainActivity.kt
@@ -114,8 +114,6 @@ fun SubscriptionCard(name: String, price: String) {
         ){
             Text(text = name, color = DarkText, fontWeight = FontWeight.Medium)
             Text(text = price, color = PastelBlue, fontWeight = FontWeight.Bold)
-
-        //selam
         }
     }
 }
